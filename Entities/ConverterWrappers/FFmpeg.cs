61dff8f656505ecdd1fcc46c7e7d85c4b9855df5
Jason Kaczmarsky <jkaczmarsky@gmail.com>
2016-01-14 23:36:27 -0500

    Add initial support for FFmpeg

--- Entities/ConverterWrappers/FFmpeg.cs
+++ Entities/ConverterWrappers/FFmpeg.cs
@@ -8,19 +8,43 @@ namespace SuperFileConverter.Entities.ConverterWrappers
 {
     public class FFmpeg:AbstractConverterWrapper
     {
+        public FFmpeg()
+        {
+            this.Name = "FFmpeg";
+            this.ExePath = new Uri(Environment.CurrentDirectory + "\\Converters\\FFmpeg\\ffmpeg.exe");
+            this.Version = this.RunCommand("-version", true)[0].Split(' ').Last();
+        }
+
         public override string ParseSettings()
         {
-            throw new NotImplementedException();
+            StringBuilder args = new StringBuilder();
+
+            if (InputFile!= null && OutputFile!=null)
+            {
+                args.AppendFormat(" -i {0}", InputFile.LocalPath);
+
+                args.AppendFormat(" -r {0}", 20);
+
+                args.AppendFormat(" {0}", OutputFile.LocalPath);
+            }
+
+            return args.ToString();
         }
 
         public override List<string> AllowedInputTypes
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return new List<string> { "webm" };
+            }
         }
 
         public override List<string> AllowedOutputTypes
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return new List<string> { "gif" };
+            }
         }
     }
 }
